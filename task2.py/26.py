45d3243f54cfefbeadc20069a19247505a154737
krishna <shahkrishna2060>
2022-06-19 16:31:26 +0530

    first commit

---
+++ task2.py/26.py
@@ -0,0 +1,21 @@
+# import pdb
+# bikel = "yamaha"
+# bikel_price = 25000
+ 
+# bike2 ="honda"
+# bike2_price = 50000
+
+# pdb.set_trace()
+# name =input("enter your name;")
+# choose = int (input("press 1 for yamaha and 2 for honda;"))
+# print (f"hello{name}")
+
+# if choose==1:
+#     print(f"{bikel} will cost you {bikel_price}")
+# elif choose ==2:
+#     print(f"{bike2} will cost you {bike2_price+2000}")
+# else:
+#     print("press only 1 or 2")
+
+
+
